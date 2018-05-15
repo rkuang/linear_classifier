90d52148d1949035991d07e18093d9ec813718a8
Ricky Kuang <rkuang26@gmail.com>
2018-05-14 17:34:57 -0700

    initial commit

---
+++ hw2.py
@@ -0,0 +1,61 @@
+# Starter code for CS 165B HW2
+import numpy
+
+def run_train_test(training_input, testing_input):
+    """
+    Implement the training and testing procedure here. You are permitted
+    to use additional functions but DO NOT change this function definition. 
+    You are you are permitted to use the numpy library but you must write 
+    your own code for the linear classifier. 
+
+    Inputs:
+        training_input: list form of the training file
+            e.g. [[3, 5, 5, 5],[.3, .1, .4],[.3, .2, .1]...]
+        testing_input: list form of the testing file
+
+    Output:
+        Dictionary of result values 
+
+        IMPORTANT: YOU MUST USE THE SAME DICTIONARY KEYS SPECIFIED
+        
+        Example:
+            return {
+                "tpr": true_positive_rate,
+                "fpr": false_positive_rate,
+                "error_rate": error_rate,
+                "accuracy": accuracy,
+                "precision": precision
+            }
+    """
+
+    # TODO: IMPLEMENT
+    
+    return -42
+
+
+#######
+# The following functions are provided for you to test your classifier.
+######
+def parse_file(filename):
+    """
+    This function is provided to you as an example of the preprocessing we do
+    prior to calling run_train_test
+    """
+    with open(filename, "r") as f:
+        data = [[float(y) for y in x.strip().split(" ")] for x in f]
+        data[0] = [int(x) for x in data[0]]
+
+        return data
+
+if __name__ == "__main__":
+    """
+    You can use this to test your code.
+    python hw2.py [training file path] [testing file path]
+    """
+    import sys
+
+    training_input = parse_file(sys.argv[1])
+    testing_input = parse_file(sys.argv[2])
+
+    print run_train_test(training_input, testing_input)
+
